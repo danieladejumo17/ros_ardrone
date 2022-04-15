1e4497d8ae79405b084dc00f0595398048414e48
Daniel <adejumodaniel17@gmail.com>
2022-04-15 16:44:15 +0100

    Initial Commit

---
+++ my_pkg/ardrone_action_client.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+from http import client
+import rospy
+import time
+import actionlib
+from ardrone_as.msg import ArdroneAction, ArdroneGoal, ArdroneFeedback, ArdroneResult
+
+nImage = 1
+
+def feedback(feedback):
+    global nImage
+    print("[Feedback] Image n.%d received"%nImage)
+    nImage += 1
+
+rospy.init_node("drone_action_client")
+
+client = actionlib.SimpleActionClient("ardrone_action_server", ArdroneAction)
+
+client.wait_for_server()
+
+goal = ArdroneGoal()
+goal.nseconds = 10
+
+client.send_goal(goal, feedback_cb=feedback)
+
+client.wait_for_result()
+
+print("[Result] State: %d"%client.get_state())
